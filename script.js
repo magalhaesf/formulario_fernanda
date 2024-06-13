a03d004699b50a14c71bf01f811c36e836cf7262
magalhaesf <160252530+magalhaesf@users.noreply.github.com>
2024-06-13 09:37:12 -0300

    modificação

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var meuFormulario = document.getElementById("cadastro_professor");
+var meuFormulario = document.getElementById("cadastro_livro");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
-  cadastrar_professor();
+  cadastrar_livro();
 });
 
-function capturarPrimeiroNumeroInteiro() {
-  const primeiro_numero_inteiro = Number(document.getElementById("nome_prof").value);
+function nome_livro() {
+  const primeiro_numero_inteiro = Number(document.getElementById("nome_livro").value);
   return primeiro_numero_inteiro;
 }
 
 function capturarSegundoNumeroInteiro() {
- const segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
+ const segundo_numero_inteiro = Number(document.getElementById("codigo_livro").value); 
  return segundo_numero_inteiro;
 }
 
@@ -19,10 +19,10 @@ function somar() {
   let primeiro_numero_inteiro = capturarPrimeiroNumeroInteiro();
   let segundo_numero_inteiro = capturarSegundoNumeroInteiro();
   let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
-  imprimirResultadoNaTela(resultado_soma);
+  imprimirResultadoNaTela(genero_textual);
 }
 
 
 function imprimirResultadoNaTela(resultado_soma) {
-  document.getElementById("resultado_soma").value = resultado_soma;
+  document.getElementById("genero_textual").value = resultado_soma;
 }
